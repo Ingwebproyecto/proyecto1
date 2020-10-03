0fa8910f3ee529d844cda53c7562faa06993001f
Valentina <vmadariagaescobar@gmail.com>
2020-10-03 00:00:54 -0300

    back end final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OfertaDespachoComponent } from './oferta-despacho/oferta-despacho.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/oferta-despacho-screen',
+    pathMatch: 'full'
+  }, // este es mi begin
+  {
+    path: 'oferta-despacho-screen',
+    component: OfertaDespachoComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/oferta-despacho-screen'
+  } //cuando se escribe cualquier cosa, te redirecciona a la pagina de inicio end
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,20 +10,47 @@ import {MatMenuModule} from '@angular/material/menu';
 import { OfertaDespachoComponent } from './oferta-despacho/oferta-despacho.component';
 import { PrincipalComponent } from './principal/principal.component';
 import { PiepaginaComponent } from './piepagina/piepagina.component';
+import { RegistroComponent } from './registro/registro.component';
+import { FormRegistroComponent } from './form-registro/form-registro.component';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+
+const router: Routes = [
+  {
+    path: 'registro',
+    component: RegistroComponent
+  },
+  {
+    path: 'formregistro',
+    component: FormRegistroComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'home',
+    component: PrincipalComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     OfertaDespachoComponent,
     PrincipalComponent,
-    PiepaginaComponent
+    PiepaginaComponent,
+    RegistroComponent,
+    FormRegistroComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSliderModule,
-    MatMenuModule
+    MatMenuModule,
+    RouterModule.forRoot(router)
   ],
   providers: [],
   bootstrap: [AppComponent]
